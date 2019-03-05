5c3955cd7bb4b6952103f0aaca820b5e216a30d2
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-03-05 07:15:31 -0500

    declared and initialized all my variables

---
+++ src/NumberGuessing.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NumberGuessing {
+
+    public static void main(String[] args){
+
+        //declare
+        int pickedNumber;
+        int enteredNumber;
+        Scanner keyboard;
+
+        //initialize
+        keyboard = new Scanner(System.in);
+
+
+
+
+
+
+    }
+}
